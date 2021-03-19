0975cd9facae91714aef7546848f737954ae3981
Florian <florian.martin63000@gmail.com>
2021-03-19 12:31:36 +0100

    Feat: WIP web hook

--- src/main/java/com/dockerize/controller/HelloController.java
+++ src/main/java/com/dockerize/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String helloController() {
-        return "Hello Worlddddd'zer WOUHOU";
+        return "Hello Worlddddd'zer WOUHOUU";
     }
 
 
