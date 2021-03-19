d3684dd0cdd2140e02d5aabec2c763b31d77fd34
Florian <florian.martin63000@gmail.com>
2021-03-19 11:02:14 +0100

    Feat: CI/CD Web Hook is up !

--- src/main/java/com/dockerize/controller/HelloController.java
+++ src/main/java/com/dockerize/controller/HelloController.java
@@ -12,4 +12,10 @@ public class HelloController {
     public String helloController() {
         return "Hello World !";
     }
+
+
+    @GetMapping("/azure")
+    public String helloController() {
+        return "Hello Azure App Service !";
+    }
 }
